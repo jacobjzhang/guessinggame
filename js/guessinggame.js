3d8ead2ed8e928902d193316ea93de1a45318208
Jacob Zhang <jacobjzhang@gmail.com>
2015-04-03 11:41:30 -0400

    Starting the javascript portion

---
+++ js/guessinggame.js
@@ -0,0 +1,18 @@
+var randnum = Math.random();
+var guessednum;
+
+$(document).ready(function(){
+  $(".guess").on("click", function(){
+    
+  });
+});
+
+$(document).ready(function(){
+  $(".guessesremaining").text(" # Remaining")
+    
+  });
+});
+
+// Check which is bigger
+// Then find the difference
+if (guessnum )
